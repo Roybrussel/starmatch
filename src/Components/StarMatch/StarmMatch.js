842de49773c832a07db422ab9fa69f3704ae1b98
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-03-11 10:28:47 +0100

    Changed class component Starmatch to a function component so that I can use the useState method. Also, the stars and numbers are now being mapped instead of being static.

--- src/Components/StarMatch/StarmMatch.js
+++ src/Components/StarMatch/StarmMatch.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import './Starmatch.scss';
 
-export default class Starmatch extends Component {
-
-    render() {
+const Starmatch = () => {
 
         // // Color Theme
         // const colors = {
@@ -44,6 +42,8 @@ export default class Starmatch extends Component {
             },
         };
 
+        const [stars, setStars] = useState(utils.random(1,9));
+
         return (
             <div className="game">
             <div className="help">
@@ -51,30 +51,17 @@ export default class Starmatch extends Component {
             </div>
             <div className="body">
               <div className="left">
-                <div className="star" />
-                <div className="star" />
-                <div className="star" />
-                <div className="star" />
-                <div className="star" />
-                <div className="star" />
-                <div className="star" />
-                <div className="star" />
-                <div className="star" />
+                {utils.range(1, stars).map(starId => 
+                    <div key={starId} className="star" />)}
               </div>
               <div className="right">
-                <button className="number">1</button>
-                <button className="number">2</button>
-                <button className="number">3</button>
-                <button className="number">4</button>
-                <button className="number">5</button>
-                <button className="number">6</button>
-                <button className="number">7</button>
-                <button className="number">8</button>
-                <button className="number">9</button>
+                {utils.range(1, 9).map(number => 
+                <button key={number} className="number">{number}</button>)}
               </div>
             </div>
             <div className="timer">Time Remaining: 10</div>
           </div>
         )
     }
-}
+
+    export default Starmatch;
